29092070c5888dccfcf6bf4b2b1f743002cffaf6
SETTER <lphp@mail.ru>
2022-08-24 04:19:04 +0300

    refactoring

--- src/cloudtipsadp/receivers.py
+++ src/cloudtipsadp/receivers.py
@@ -17,14 +17,17 @@ class Receiver:
 
     def __init__(self, phone_number: str):
         self.phone_number = phone_number
-        # self.url = Connect.client.api(list(args))
-
-    # @classmethod
-    # def api_url(cls, *args):
-    #     return Connect.client.api(list(args))
+        self.header = Connect.get_headers()
 
     def __call__(self, *args, **kwargs):
         return Connect.client.api(list(args))
+
+    def _post(self, url, data: dict = dict()):
+        return requests.post(url, data=json.dumps(data),
+                             headers=self.header).json()
+
+    def _get(self, url, params: dict = dict()):
+        return requests.get(url, params=params, headers=self.header).json()
 
     def create(self):
         raise NotImplementedError(M_BASE_IMPLEMENTED)
@@ -57,21 +60,18 @@ class Receivers(Receiver):
         except AttributeError:
             print('No user data.')
         else:
-            return json.dumps(data)
+            return data
 
     def create(self):
         """Создать получателя донатов в сервисе."""
-        api_url = self(self.base_path, 'create-many')
-        parsed = requests.post(api_url, data=self.__get_data(),
-                               headers=Connect.get_headers()).json()
-        return parsed
+        url = self(self.base_path, 'create-many')
+        return self._post(url, self.__get_data())
 
     def detach_agent(self):
         """Удалить получателя из скоупа."""
         try:
-            api_url = self(self.base_path, self.user_id, 'detach-agent')
-            parsed = requests.post(api_url,
-                                   headers=Connect.get_headers()).json()
+            url = self(self.base_path, self.user_id, 'detach-agent')
+            parsed = self._post(url)
         except TypeError:
             print('NotFound user_id.')
         else:
@@ -80,8 +80,7 @@ class Receivers(Receiver):
     def pages(self):
         """Все получатели в заведении."""
         api_url = self(self.base_path)
-        parsed = requests.get(api_url, headers=Connect.get_headers()).json()
-        return parsed
+        return self._get(api_url)
 
     def filter(self, ):
         """Выборка данных согласно параметрам запроса."""
@@ -102,12 +101,11 @@ class Receivers(Receiver):
         except FileNotFoundError as e:
             print(f'{FILE_PATH_BAD} {e}')
         else:
-            api_url = self(self.base_path, self.user_id, 'photo')
-            parsed = requests.post(api_url,
-                                   headers=Connect.get_headers_token(),
-                                   data=payload,
-                                   files=files)
-            return parsed.json()
+            url = self(self.base_path, self.user_id, 'photo')
+            parsed = requests.post(
+                url, headers=Connect.get_headers_token(), data=payload,
+                files=files).json()
+            return parsed
 
 
 if __name__ == '__main__':
@@ -119,103 +117,109 @@ if __name__ == '__main__':
 
     name = 'Poale Ell Adam'
     phone_number = '+79162047558'
-    ob = cta.receivers_create(cta.receivers(name, phone_number))
-    if type(ob) == dict and ob.get('succeed'):
+    response = cta.receivers_create(cta.receivers(name, phone_number))
+    if type(response) == dict and response.get('succeed'):
         print('Получатель донатов создан:')
-        print(ob.get('data'))
-        # obg = ob.get('data')
+        print(response.get('data'))
+        # obg = response.get('data')
         # print(obg)
-        user_id = ob.get('data')['created'][0]['userId']
+        user_id = response.get('data')['created'][0]['userId']
         print(f'USER ID: {user_id}')
     else:
-        print(f'ERROR**: {ob}')
+        print(f'ERROR**: {response}')
 
-    ob = cta.places_send_sms(cta.places(user_id=user_id))
-    if type(ob) == dict and ob.get('succeed'):
+    response = cta.places_send_sms(cta.places(user_id=user_id))
+    if type(response) == dict and response.get('succeed'):
         print(
             f'Отправлено сообщении код в смс на телефон получателя {user_id}')
-        print(ob)
-        ob = cta.places_confirm(cta.places(user_id=user_id, code=000000))
-        if type(ob) == dict and ob.get('succeed'):
+        print(response)
+        response = cta.places_confirm(cta.places(user_id=user_id, code=000000))
+        if type(response) == dict and response.get('succeed'):
             print(
                 f'Отправлен код из SMS получателя: {user_id}')
-            print(ob)
+            print(response)
         else:
-            print(f'ERROR код: {ob}')
+            print(f'ERROR код: {response}')
     else:
-        print(f'ERROR все: {ob}')
+        print(f'ERROR все: {response}')
 
-    user_id = '44a38440-595d-494e-a028-09804355757a'
-    ob = cta.receivers_detach_agent(cta.receivers(user_id=user_id))
-    if type(ob) == dict and ob.get('succeed'):
+    # user_id = '44a38440-595d-494e-a028-09804355757a'
+    response = cta.receivers_detach_agent(cta.receivers(user_id=user_id))
+    if type(response) == dict and response.get('succeed'):
         print(f'Удалён получатель {user_id} из скоупа:')
-        print(ob)
+        print(response)
     else:
-        print(f'ERROR все: {ob}')
+        print(f'ERROR все: {response}')
 
+    response = cta.receivers_pages(cta.receivers())
+    if type(response) == dict and response.get('succeed'):
+        print('Все получатели донатов:')
+        print(response.get('data'))
+    else:
+        print(f'ERROR donations: {response}')
     # photo_path = '/home/setter/Изображения/Рецепты/1.png'
-    # ob = cta.receivers_photo(cta.receivers(user_id=user_id,
+    # response = cta.receivers_photo(cta.receivers(user_id=user_id,
     #                                        photo_path=photo_path))
-    # if type(ob) == dict and ob.get('succeed'):
+    # if type(response) == dict and response.get('succeed'):
     #     print('Фото получателя успешно загружено:')
-    #     print(ob.get('data'))
+    #     print(response.get('data'))
     # else:
-    #     print(f'ERROR все: {ob}')
+    #     print(f'ERROR все: {response}')
 
-    # ob = cta.receivers_pages(cta.receivers())
-    # if ob.get('succeed'):
+    # response = cta.receivers_pages(cta.receivers())
+    # if response.get('succeed'):
     #     print('Все получатели в системе:')
-    #     print(ob.get('data'))
+    #     print(response.get('data'))
     # else:
-    #     print(f'ERROR все: {ob}')
+    #     print(f'ERROR все: {response}')
 
-    # ob = cta.receivers_create(cta.receivers('Adam', '+79162047558'))
-    # ob = cta.receivers_create(cta.receivers('Ozzy Osbourne', '+72002040001'))
-    # ob = cta.receivers_create(cta.receivers('AC/DC', '+71002040007'))
-    # print(ob)
+    # response = cta.receivers_create(cta.receivers('Adam', '+79162047558'))
+    # response = cta.receivers_create(cta.receivers('Ozzy Osbourne', '+72002040001'))
+    # response = cta.receivers_create(cta.receivers('AC/DC', '+71002040007'))
+    # print(response)
 
     # Получатель уже есть в системе, но не в нашем скоупе.
-    # ob = {'data': {'created': [{'phoneNumber': '+72002040005',
+    # response = {'data': {'created': [{'phoneNumber': '+72002040005',
     # 'name': 'Foo-55', 'userId': '21591c56-dfc6-432d-93af-882c0ea454aa',
     # 'layoutId': 'a454aabd', 'layoutStatus': 'WaitingForConfirmation'}],
     # 'skipped': []}, 'succeed': True, 'errors': None,
     # 'validationErrors': None}
 
     # Новый получатель создан
-    # ob = {'data': {'created': [{'phoneNumber': '+72002040005',
+    # response = {'data': {'created': [{'phoneNumber': '+72002040005',
     # 'name': 'Foo-55', 'userId': '21591c56-dfc6-432d-93af-882c0ea454aa',
     # 'layoutId': 'a454aabd', 'layoutStatus': 'None'}], 'skipped': []},
     # 'succeed': True, 'errors': None, 'validationErrors': None}
 
     # Получатель уже есть в системе и находится в вашем скоупе,так как
     # находится в массиве skipped:
-    # ob = {'data': {'created': [], 'skipped': [{'phoneNumber': '+79162047558',
+    # response = {'data': {'created': [], 'skipped': [{'phoneNumber': '+79162047558',
     # 'name': 'Adam', 'userId': '44a38440-595d-494e-a028-09804355757a',
     # 'layoutId': '55757a3e', 'layoutStatus': 'None'}]}, 'succeed': True,
     # 'errors': None, 'validationErrors': None}
 
-    # if ob.get('succeed'):
-    #     if len(ob.get('data')['skipped']) > 0:
+    # if response.get('succeed'):
+    #     if len(response.get('data')['skipped']) > 0:
     #         print('Получатель уже есть в системе и находится в вашем скоупе,'
     #               'так как находится в массиве skipped: ')
-    #         print(ob.get('data')['skipped'])
-    #     elif len(ob.get('data')['created']) > 0 and \
-    #             ob.get('data')['created'][0][
+    #         print(response.get('data')['skipped'])
+    #     elif len(response.get('data')['created']) > 0 and \
+    #             response.get('data')['created'][0][
     #                 'layoutStatus'] == 'WaitingForConfirmation':
     #         print('Получатель уже есть в системе, но не в нашем скоупе.')
     #         print('Так как есть в массиве created и '
     #               'layoutStatus = WaitingForConfirmation:')
-    #         print(ob.get('data')['created'])
+    #         print(response.get('data')['created'])
     #         print('Получатель должен подтвердить свое желание, что вы его '
     #               'привяжете к своему скоупу.')
     #         print('Для подтверждение, ему надо направить sms.')
-    #     elif len(ob.get('data')['created']) > 0 and \
-    #             ob.get('data')['created'][0]['layoutStatus'] == 'None':
+    #     elif len(response.get('data')['created']) > 0 and \
+    #             response.get('data')['created'][0]['layoutStatus'] == 'None':
     #         print('Получатель не был в системе и сейчас был создан и '
     #               'добавлен в ваш скоуп.')
-    #         print(ob.get('data'))
+    #         print(response.get('data'))
     #     else:
     #         print(f'Получатель донатов создан.')
-    #         print(ob.get('data'))
+    #         print(response.get('data'))
     # else:
-    #     print(f'ERROR: {ob}')
+    #     print(f'ERROR: {response}')
